3e646d66bdcb7f65332ea89c941d7c3b23367d1b
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-05-22 14:18:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿    using Amazon.S3;
+    using Amazon.S3.Model;
+
+    public class S3Example
+    {
+        public static async Task Main()
+        {
+            string bucketName = "sample3bucket";
+            string keyName = "samplefile.txt";
+
+            IAmazonS3 client = new AmazonS3Client();
+
+            await WritingAnObjectAsync(client, bucketName, keyName);
+        }
+
+        public static async Task WritingAnObjectAsync(IAmazonS3 client, string bucketName, string keyName)
+        {
+            try
+            {
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    ContentBody = "sample text",
+                    ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256,
+                };
+
+                var putResponse = await client.PutObjectAsync(putRequest);
+
+                // Determine the encryption state of an object.
+                GetObjectMetadataRequest metadataRequest = new GetObjectMetadataRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                };
+                GetObjectMetadataResponse response = await client.GetObjectMetadataAsync(metadataRequest);
+                ServerSideEncryptionMethod objectEncryption = response.ServerSideEncryptionMethod;
+
+                Console.WriteLine($"Encryption method used: {0}", objectEncryption.ToString());
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error: '{ex.Message}' when writing an object");
+            }
+        }
+    }
+
+
